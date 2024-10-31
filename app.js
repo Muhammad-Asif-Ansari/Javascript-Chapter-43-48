2dcbc0a6f0828531af4420d03dbd0ba54f5cd2ce
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-10-31 21:56:45 +0500

    Assignment
    Assignment

---
+++ app.js
@@ -0,0 +1,37 @@
+// Question no 01:
+// function showPopup(browserpopup){
+//     alert(browserpopup)
+// }
+
+// Question no 02:
+// function showMessage(mobileNames){
+//     alert(mobileNames);
+    
+// }
+
+
+//  Question no 03:
+
+// function deleteRow(button) {
+//     var row = button.parentNode.parentNode;
+//     row.parentNode.removeChild(row);
+//     }
+
+
+//  Question no 05:
+
+// let counter = 0; 
+
+// function updateCounterDisplay() {
+//     document.getElementById("counterValue").innerText = counter;
+// }
+
+// function increaseCounter() {
+//     counter++;
+//     updateCounterDisplay();
+// }
+
+// function decreaseCounter() {
+//     counter--;
+//     updateCounterDisplay();
+// }
